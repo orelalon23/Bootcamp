d19a08fca1aeaf8b791fc85d14ab0c7f62daa340
Orel Alon <orela231089@gmail.com>
2021-02-07 15:04:00 +0200

    first commit

---
+++ javascript/1.1.js
@@ -0,0 +1,25 @@
+
+let name = 'orel';
+const age = 31;
+const me = {
+    first: 'OREL',
+    last: 'ALON',
+    age: 31
+};
+
+let work = true;
+
+let orelHusband = null;
+
+let orelCar;
+
+const symb = Symbol('something');
+
+
+console.log(name);
+console.log(age);
+console.log(me);
+console.log(work);
+console.log(orelHusband);
+console.log(orelCar);
+console.log(symb);
---
+++ javascript/1.2.js
@@ -0,0 +1,48 @@
+/*
+--Delete the wrong answers--
+1. Which of the following is/are strings?
+a) 4
+b) 4.0
+c) '4'
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+c) '4'
+d) -4
+3. Which of the following is/are booleans?
+a) true
+b) false
+c) "true"
+4. What is the result of 80 + 71.2?
+a) 151.2
+b) 151
+c) 8071.2
+5. What is the result of "80" + 71.2?
+a) 151.2
+b) 151
+c) "8071.2"
+6. Does 100 + 30 produce a number or a string?
+a) number
+b)string
+7. Does "100" + 30 produce a number or a string?
+a)number
+b)string
+*/
+
+//------Submit your answers under the question------//
+
+//create three different ways to declare variables
+
+//declare a variable and reassign a value to it
+
+//create a variable and after that assign a string with its value being: He's got it!
+
+/*
+1. create a variable and assign a value on how much a restaurant bill is.
+2. create a variable and assign a value on how much tax they should pay.
+3. create a variable that will calculate the bill * tax and its output would be: Your total bill is <total bill> $.
+ */
+
+// Round the number 50.6 to its nearest integer
+
+//Create a variable that is undefined
